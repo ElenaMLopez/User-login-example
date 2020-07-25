c48450eb2a29c5fcced64810376b216af2d1ee1d
elenamlopez <elena.mateos1@gmail.com>
2020-07-25 19:26:16 +0200

    Add component and view for login

---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login-container">
+    <img alt="Karumi logo" src="../assets/logo.png" />
+    <LoginForm />
+  </div>
+</template>
+<script>
+import LoginForm from "@/components/LoginForm.vue";
+
+export default {
+  name: "Login",
+  components: {
+    LoginForm
+  }
+};
+</script>
+<style lang="scss" scoped>
+.login-container {
+  border-radius: 5px;
+  border: 1.5px solid #cccccc;
+  margin: 100px auto;
+  padding: 100px 40px 20px;
+  width: 280px;
+}
+</style>
