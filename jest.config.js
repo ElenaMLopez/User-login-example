e00eadbc46bce9f079eb80f2c0db4b097a38935a
elenamlopez <elena.mateos1@gmail.com>
2020-07-25 19:39:40 +0200

    Update jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest"
+  verbose: true,
+  preset: '@vue/cli-plugin-unit-jest',
+  mocks: './test/unit/mocks',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!src/main.js', // No need to cover bootstrap file
+  ],
 };
